097eef0534d053aec14aa34cf3c4b02576595d16
ansariali <=>
2025-06-13 16:23:22 +0530

    otp model done

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,6 +16,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { createAccount } from "@/lib/action/user.action";
 import { useState } from "react";
+import { OtpModel } from "./OtpModel";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -30,7 +31,7 @@ const authFormSchema = (formType: FormType) => {
 };
 
 export default function AuthForm({ type }: { type: FormType }) {
-  const [accountId, setAccountId] = useState(null);
+  const [accountId, setAccountId] = useState("null");
   const [isError, setIsError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
@@ -61,24 +62,45 @@ export default function AuthForm({ type }: { type: FormType }) {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <h1 className="text-2xl font-bold white">
-          {type === "sign-up" ? "Sign Up" : "Sign In"}
-        </h1>
-        {type === "sign-up" && (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <h1 className="text-2xl font-bold white">
+            {type === "sign-up" ? "Sign Up" : "Sign In"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullname"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2 w-90 white">
+                  <FormLabel className="text-light-100 pt-2 body-2 w-full">
+                    Fullname:{" "}
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="order-none shadow-none p-2 outline-none ring-offset-transparent focus:ring-transparent focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 placeholder:text-light-200 body-2"
+                      placeholder="Type Your Fullname"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
           <FormField
             control={form.control}
-            name="fullname"
+            name="email"
             render={({ field }) => (
               <FormItem className="flex flex-col gap-2 w-90 white">
                 <FormLabel className="text-light-100 pt-2 body-2 w-full">
-                  Fullname:{" "}
+                  Email:{" "}
                 </FormLabel>
                 <FormControl>
                   <Input
                     className="order-none shadow-none p-2 outline-none ring-offset-transparent focus:ring-transparent focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 placeholder:text-light-200 body-2"
-                    placeholder="Type Your Fullname"
+                    placeholder="Email Address here"
                     {...field}
                   />
                 </FormControl>
@@ -86,35 +108,19 @@ export default function AuthForm({ type }: { type: FormType }) {
               </FormItem>
             )}
           />
-        )}
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem className="flex flex-col gap-2 w-90 white">
-              <FormLabel className="text-light-100 pt-2 body-2 w-full">
-                Email:{" "}
-              </FormLabel>
-              <FormControl>
-                <Input
-                  className="order-none shadow-none p-2 outline-none ring-offset-transparent focus:ring-transparent focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 placeholder:text-light-200 body-2"
-                  placeholder="Email Address here"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button
-          disabled={isLoading}
-          className="bg-red-400 hover:bg-red-300 w-full cursor-pointer transition-all rounded-full"
-          type="submit"
-        >
-          {isLoading ? "Please wait" : "submit"}
-        </Button>
-      </form>
-      {isError && <p>Error: {isError}</p>}
-    </Form>
+          <Button
+            disabled={isLoading}
+            className="bg-red-400 hover:bg-red-300 w-full cursor-pointer transition-all rounded-full"
+            type="submit"
+          >
+            {isLoading ? "Please wait" : "submit"}
+          </Button>
+        </form>
+        {isError && <p>Error: {isError}</p>}
+      </Form>
+      {true && (
+        <OtpModel email={form.getValues("email")} accountId={accountId} />
+      )}
+    </>
   );
 }
---
+++ components/OtpModel.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "./ui/alert-dialog";
+import { Button } from "./ui/button";
+import { InputOTP, InputOTPGroup, InputOTPSlot } from "./ui/input-otp";
+
+export const OtpModel = ({
+  accountId,
+  email,
+}: {
+  email: string;
+  accountId: string;
+}) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("password: ", password, "accountId: ", accountId);
+    setIsOpen(false);
+  };
+
+  return (
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogContent className="flex flex-col items-center justify-center py-12">
+        <AlertDialogHeader className="relative flex flex-col items-center justify-center">
+          <AlertDialogTitle className="">Enter Your OTP</AlertDialogTitle>
+          <p
+            onClick={() => setIsOpen(false)}
+            className="absolute top-[-20px] right-[-40px] text-2xl cursor-pointer"
+          >
+            &times;
+          </p>
+          <AlertDialogDescription>
+            We&apos;ve sent a code to{" "}
+            <span>{"ansariali.developer@gmail.com"}</span>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <InputOTP maxLength={6} value={password} onChange={setPassword}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} className="border-zinc-400" />
+            <InputOTPSlot index={1} className="border-zinc-400" />
+            <InputOTPSlot index={2} className="border-zinc-400" />
+            <InputOTPSlot index={3} className="border-zinc-400" />
+            <InputOTPSlot index={4} className="border-zinc-400" />
+            <InputOTPSlot index={5} className="border-zinc-400" />
+          </InputOTPGroup>
+        </InputOTP>
+        <AlertDialogFooter className="grid grid-cols-1">
+          <AlertDialogAction onClick={handleSubmit} type="button">
+            {isLoading ? "Please wait" : "Submit"}
+          </AlertDialogAction>
+          <div>
+            Didn&apos;t get a code?
+            <Button type="button" variant={"link"}>
+              click to resend
+            </Button>
+          </div>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
