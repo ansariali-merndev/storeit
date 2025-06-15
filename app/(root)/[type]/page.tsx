8cb90472aeb6734f4264613afc45f1ddde462a35
ansariali <=>
2025-06-15 13:41:20 +0530

    done

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,7 +1,25 @@
-export default function Type() {
+import { Card } from "@/components/Card";
+import { getUserFiles } from "@/lib/action/file.action";
+
+export default async function Type({ params }: { params: any }) {
+  const { type } = await params;
+  let filteredFile = await getUserFiles();
+
+  if (type === "images") {
+    filteredFile = filteredFile.filter((item) => item.type === "image");
+  } else if (type === "documents") {
+    filteredFile = filteredFile.filter((item) => item.type === "document");
+  } else if (type === "media") {
+    filteredFile = filteredFile.filter(
+      (item) => item.type === "video" || item.type === "audio"
+    );
+  } else if (type === "others") {
+    filteredFile = filteredFile.filter((item) => item.type === "other");
+  }
+
   return (
     <section>
-      <h2>Hello wporld</h2>
+      <Card file={filteredFile} />
     </section>
   );
 }
